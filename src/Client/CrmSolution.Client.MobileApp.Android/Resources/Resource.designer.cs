741d4af854ac1573e842f3b0466f814a835fa70f
Yaser Moradi <ysmoradi@outlook.com>
2020-08-14 16:08:43 +0430

    Updated to bit 5.7.2

--- src/Client/CrmSolution.Client.MobileApp.Android/Resources/Resource.designer.cs
+++ src/Client/CrmSolution.Client.MobileApp.Android/Resources/Resource.designer.cs
@@ -8825,6 +8825,7 @@ namespace CrmSolution.Client.MobileApp.Droid
 			global::Xamarin.Forms.Platform.Android.Resource.String.fab_transformation_sheet_behavior = global::CrmSolution.Client.MobileApp.Droid.Resource.String.fab_transformation_sheet_behavior;
 			global::Xamarin.Forms.Platform.Android.Resource.String.hide_bottom_view_on_scroll_behavior = global::CrmSolution.Client.MobileApp.Droid.Resource.String.hide_bottom_view_on_scroll_behavior;
 			global::Xamarin.Forms.Platform.Android.Resource.String.mtrl_chip_close_icon_content_description = global::CrmSolution.Client.MobileApp.Droid.Resource.String.mtrl_chip_close_icon_content_description;
+			global::Xamarin.Forms.Platform.Android.Resource.String.overflow_tab_title = global::CrmSolution.Client.MobileApp.Droid.Resource.String.overflow_tab_title;
 			global::Xamarin.Forms.Platform.Android.Resource.String.password_toggle_content_description = global::CrmSolution.Client.MobileApp.Droid.Resource.String.password_toggle_content_description;
 			global::Xamarin.Forms.Platform.Android.Resource.String.path_password_eye = global::CrmSolution.Client.MobileApp.Droid.Resource.String.path_password_eye;
 			global::Xamarin.Forms.Platform.Android.Resource.String.path_password_eye_mask_strike_through = global::CrmSolution.Client.MobileApp.Droid.Resource.String.path_password_eye_mask_strike_through;
@@ -11503,6 +11504,7 @@ namespace CrmSolution.Client.MobileApp.Droid
 			global::Xamarin.Forms.Platform.Resource.String.fab_transformation_sheet_behavior = global::CrmSolution.Client.MobileApp.Droid.Resource.String.fab_transformation_sheet_behavior;
 			global::Xamarin.Forms.Platform.Resource.String.hide_bottom_view_on_scroll_behavior = global::CrmSolution.Client.MobileApp.Droid.Resource.String.hide_bottom_view_on_scroll_behavior;
 			global::Xamarin.Forms.Platform.Resource.String.mtrl_chip_close_icon_content_description = global::CrmSolution.Client.MobileApp.Droid.Resource.String.mtrl_chip_close_icon_content_description;
+			global::Xamarin.Forms.Platform.Resource.String.overflow_tab_title = global::CrmSolution.Client.MobileApp.Droid.Resource.String.overflow_tab_title;
 			global::Xamarin.Forms.Platform.Resource.String.password_toggle_content_description = global::CrmSolution.Client.MobileApp.Droid.Resource.String.password_toggle_content_description;
 			global::Xamarin.Forms.Platform.Resource.String.path_password_eye = global::CrmSolution.Client.MobileApp.Droid.Resource.String.path_password_eye;
 			global::Xamarin.Forms.Platform.Resource.String.path_password_eye_mask_strike_through = global::CrmSolution.Client.MobileApp.Droid.Resource.String.path_password_eye_mask_strike_through;
@@ -17187,25 +17189,28 @@ namespace CrmSolution.Client.MobileApp.Droid
 			public const int mtrl_chip_close_icon_content_description = 2131558435;
 			
 			// aapt resource value: 0x7F0D0024
-			public const int password_toggle_content_description = 2131558436;
+			public const int overflow_tab_title = 2131558436;
 			
 			// aapt resource value: 0x7F0D0025
-			public const int path_password_eye = 2131558437;
+			public const int password_toggle_content_description = 2131558437;
 			
 			// aapt resource value: 0x7F0D0026
-			public const int path_password_eye_mask_strike_through = 2131558438;
+			public const int path_password_eye = 2131558438;
 			
 			// aapt resource value: 0x7F0D0027
-			public const int path_password_eye_mask_visible = 2131558439;
+			public const int path_password_eye_mask_strike_through = 2131558439;
 			
 			// aapt resource value: 0x7F0D0028
-			public const int path_password_strike_through = 2131558440;
+			public const int path_password_eye_mask_visible = 2131558440;
 			
 			// aapt resource value: 0x7F0D0029
-			public const int search_menu_title = 2131558441;
+			public const int path_password_strike_through = 2131558441;
 			
 			// aapt resource value: 0x7F0D002A
-			public const int status_bar_notification_info_overflow = 2131558442;
+			public const int search_menu_title = 2131558442;
+			
+			// aapt resource value: 0x7F0D002B
+			public const int status_bar_notification_info_overflow = 2131558443;
 			
 			static String()
 			{
